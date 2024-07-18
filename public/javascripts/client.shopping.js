5b1c7383e22b980c3bd579e377aa79823dded348
Julivan <julivan.santos@nubank.com.br>
2024-07-17 21:03:40 -0300

    adding shopping test cases

--- public/javascripts/client.shopping.js
+++ public/javascripts/client.shopping.js
@@ -3,24 +3,37 @@
     var isSized = location.search == '?sized' || location.search == '?sized=true' && location.search != '?sized=false';
     var isGitHubHost = location.href.indexOf('github') != -1;
 
-    if (isCurrent && !isSized) {
-        document.getElementById('main-container').classList.remove('size-containers')
+    if (isSized) {
+        document.getElementById('main-container').classList.add('size-containers')
+        document.getElementById('main-container').classList.add('fade')
+        document.getElementById('main-container').classList.remove('without-fade')
     }
 
     setTimeout(() => {
-        makeRequest('header');
-        makeRequest('account-balance');
+        makeRequest('search');
     }, 400)
     
     setTimeout(() => {
-        makeRequest('inline-actions');
-    }, 500)
+        makeRequest('shopping-actions');
+    }, 800)
 
     setTimeout(() => {
-        makeRequest('banners');
-        makeRequest('credit-card-balance');
-        makeRequest('lending');
-    }, 800)
+        makeRequest('sellers-carousel');
+    }, 950)
+
+    setTimeout(() => {
+        makeRequest('highlight-by-department');
+    }, 1100)
+
+    setTimeout(() => {
+        makeRequest('highlight-banners');
+    }, 1800)
+    
+    setTimeout(() => {
+        makeRequest('shopping-banner');
+    }, 2000)
+
+    
 
     function makeRequest (dataType) {
         var url = isGitHubHost ? `/flickering-test/${dataType}` : `/${dataType}`;
@@ -30,7 +43,15 @@
             .then(data => {
                 var container = document.getElementById(dataType);
 
-                container.innerHTML = data;
+                if (!isSized) {
+                    container.innerHTML = "";
+                    setTimeout(() => {
+                        container.innerHTML = data;
+                        container.getElementsByClassName('fade')[0].classList.remove('hide');
+                    }, 100);
+                }else {
+                    container.innerHTML = data;
+                }
                 
                 if (isCurrent) {
                     container.getElementsByClassName('fade')[0].classList.remove('hide');
--- routes/index.js
+++ routes/index.js
@@ -41,4 +41,28 @@ router.get('/without-skeleton-fade', function(req, res, next) {
   res.render('without-skeleton-fade');
 });
 
+router.get('/sellers-carousel', function(req, res, next) {
+  res.render('sellers-carousel');
+});
+
+router.get('/highlight-by-department', function(req, res, next) {
+  res.render('highlight-by-department');
+});
+
+router.get('/highlight-banners', function(req, res, next) {
+  res.render('highlight-banners');
+});
+
+router.get('/search', function(req, res, next) {
+  res.render('search');
+});
+
+router.get('/shopping-banner', function(req, res, next) {
+  res.render('shopping-banner');
+});
+
+router.get('/shopping-actions', function(req, res, next) {
+  res.render('shopping-actions');
+});
+
 module.exports = router;
